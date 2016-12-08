d8cb4aefd26f65828b5b1cd3880f4fcdde29d50d
kshitizlondon <mahajan.london@yahoo.com>
2016-12-08 16:19:06 +0000

    added more elements to the dictionary

--- fundas/dictionary.py
+++ fundas/dictionary.py
@@ -13,7 +13,7 @@ def create_new_person():
     }
 
 
-def add_more_about_person(person, **kwargs):
+def add_more_details_about_person(person, **kwargs):
     for more_detail in kwargs:
         person[more_detail] = kwargs[more_detail]
 
@@ -35,11 +35,23 @@ def remove_age_detail(person):
     return person
 
 
+def add_marks(person, **kwargs):
+    """ added more elements to the dictionary """
+    if 'marks' in person:
+        person['marks'].update(kwargs)
+    else:
+        person['marks'] = kwargs
+
+    return person
+
+
 def person_details():
     person = create_new_person()
-    person = add_more_about_person(person, address='London', status='Single')
+    person = add_more_details_about_person(person, address='London', status='Single')
     person = add_friends(person, 'Marry', 'Martin')
     person = remove_age_detail(person)
+    person = add_marks(person, English=50, Maths=100)
+    person = add_marks(person, Science=50, Marketing=100)
 
     for about_person in person:
         print(about_person, person[about_person])
