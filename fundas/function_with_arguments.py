081c4505ce0fbee96ee8a83859063e7474a8bf0b
kshitizlondon <mahajan.london@yahoo.com>
2016-12-08 11:09:07 +0000

    functions with arguments

---
+++ fundas/function_with_arguments.py
@@ -0,0 +1,37 @@
+import inspect
+
+
+def whoami():
+    return inspect.stack()[1][3]
+
+
+def function_with_normal_arguments(some_arg1, some_arg2, *args):
+    print('Results from {}:'.format(whoami()))
+    return some_arg1, some_arg2, args
+
+
+def function_with_normal_and_unknown_arguments(some_arg1, some_arg2, *args):
+    print('Results from {}:'.format(whoami()))
+    return some_arg1, some_arg2, args
+
+
+def function_with_normal_and_unknown_arguments_plus_key_value_args(
+        some_arg1, some_arg2, *args, **kwargs):
+    print('Results from {}:'.format(whoami()))
+    return some_arg1, some_arg2, args, kwargs
+
+
+if __name__ == '__main__':
+
+    print(function_with_normal_arguments('apple', 'mango'))
+
+    print(function_with_normal_arguments(
+        'apple', 'mango',  # passing *args, will be printed in tuple
+        'apple_args', 'mango_args')
+    )
+
+    print(function_with_normal_and_unknown_arguments_plus_key_value_args(
+        'apple', 'mango',  # passing normal arguments
+        'apple_args','mango_args',  # passing *args, will be printed in tuple
+        first_name="John", last_name="Doe")  # passing *kwargs, will be printed in dict
+    )
